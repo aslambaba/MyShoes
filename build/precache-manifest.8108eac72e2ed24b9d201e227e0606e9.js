935f4fde09439686b22fb9c07f80e8bd2eee712a
github_workflow <aslam91r@gmail.com>
2022-12-07 16:47:41 +0000

    Add Build Image

---
+++ build/precache-manifest.8108eac72e2ed24b9d201e227e0606e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f48b612b2b8484df3bdcda6b81cf773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3ed92be98fa31c3c98",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49eb4b939a9d8120f791",
+    "url": "/static/js/2.34d7dacc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.34d7dacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3ed92be98fa31c3c98",
+    "url": "/static/js/main.aab64d5e.chunk.js"
+  },
+  {
+    "revision": "2c65b18177cbfc047093",
+    "url": "/static/js/runtime-main.a85d9f65.js"
+  }
+]);
